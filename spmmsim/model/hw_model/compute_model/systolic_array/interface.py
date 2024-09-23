0ee12c0dfb8bf8595f2414aa354145d59d2e5d4b
shiroha <whmio0115@gmail.com>
2024-09-24 00:59:41 +0800

    [WIP]

--- spmmsim/model/hw_model/compute_model/systolic_array/interface.py
+++ spmmsim/model/hw_model/compute_model/systolic_array/interface.py
@@ -4,26 +4,63 @@ from SparseCompution import SparseCompution
 from utils.SparseMatirx import SparseMatrix
 from utils.IndexBuffer import IndexBuffer
 
-def matmul_eval(systolic_row, systolic_col, matrix_A, matrix_B, mode=0, Sparsity=False):
-    if Sparsity:
-        buffer_width = [2, 4, 8, 16, 32]
-        cycles = []
-        for i in buffer_width:
-            sparse_compute = SparseCompution(systolic_row, i, systolic_col)
-            sparse_compute.systolic_array.reset()
-            c = sparse_compute.sparse_compute(matrix_A, matrix_B, mode)
-            cycle = sparse_compute.systolic_array.cycle
-            cycles.append(cycle)
-            print(f"脉动阵列为{systolic_row}x{systolic_col}, buf宽度为{i}, 填充方式为{mode}, cycle为{cycle}")
+
+def matmul_eval(matrix_A, matrix_B, systolic_row_opt=-1, systolic_col_opt=-1, 
+                buffer_col_opt=-1, mode=0, Sparsity=False):
+    if Sparsity:        
+        sparse_compute = SparseCompution(systolic_row_opt, buffer_col_opt, systolic_col_opt)
+        sparse_compute.systolic_array.reset()
+        c = sparse_compute.sparse_compute(matrix_A, matrix_B, mode)
+        cycle = sparse_compute.systolic_array.cycle
+        print(f"脉动阵列为{systolic_row_opt}x{systolic_col_opt}, 稀疏矩阵填充方式为{mode}, cycle为{cycle}")
         return cycle
     else:
-        dense_compute = SparseCompution(systolic_row, 0, systolic_col)
-        # dense_compute = SystolicArray(systolic_row, systolic_col)
+        dense_compute = SparseCompution(systolic_row_opt, buffer_col_opt, systolic_col_opt)
         dense_compute.systolic_array.reset()
         c = dense_compute.dense_compute(matrix_A.matrix, matrix_B)
-        print("密集矩阵计算cycle:", dense_compute.cycle)
-        return dense_compute.cycle
+        print(f"脉动阵列为{systolic_row_opt}x{systolic_col_opt}, 密集矩阵计算cycle:", dense_compute.systolic_array.cycle)
+        return dense_compute.systolic_array.cycle
+
+def memory_access_generate(matrix_A, matrix_B, systolic_row_opt=-1, systolic_col_opt=-1, 
+                buffer_col_opt=-1, mode=0, Sparsity=False):
+    if Sparsity:        
+        sparse_compute = SparseCompution(systolic_row_opt, buffer_col_opt, systolic_col_opt)
+        sparse_compute.systolic_array.reset()
+        c = sparse_compute.sparse_compute(matrix_A, matrix_B, mode)
+        cycle = sparse_compute.systolic_array.cycle
+        print(f"脉动阵列为{systolic_row_opt}x{systolic_col_opt}, 稀疏矩阵填充方式为{mode}, cycle为{cycle}")
+        return cycle
+    else:
+        dense_compute = SparseCompution(systolic_row_opt, buffer_col_opt, systolic_col_opt)
+        dense_compute.systolic_array.reset()
+        c = dense_compute.dense_compute(matrix_A.matrix, matrix_B)
+        print(f"脉动阵列为{systolic_row_opt}x{systolic_col_opt}, 密集矩阵计算cycle:", dense_compute.systolic_array.cycle)
+        return dense_compute.systolic_array.cycle
 
 A = SparseMatrix(2048, 2048, 1/16)
 B = np.random.randint(0, 10, size=(2048, 128))
 
+# 测试跳了一个参数
+matmul_eval(A, B, 32,  4, mode=0, Sparsity=True)
+matmul_eval(A, B, 32,  4, mode=1, Sparsity=False)
+matmul_eval(A, B, 32,  4, mode=1, Sparsity=True)
+matmul_eval(A, B, 32,  8, mode=0, Sparsity=False)
+matmul_eval(A, B, 32,  8, mode=0, Sparsity=True)
+matmul_eval(A, B, 32,  8, mode=1, Sparsity=False)
+matmul_eval(A, B, 32,  8, mode=1, Sparsity=True)
+matmul_eval(A, B, 32, 16, mode=0, Sparsity=False)
+matmul_eval(A, B, 32, 16, mode=0, Sparsity=True)
+matmul_eval(A, B, 32, 16, mode=1, Sparsity=False)
+matmul_eval(A, B, 32, 16, mode=1, Sparsity=True)
+matmul_eval(A, B,  4, 32, mode=0, Sparsity=False)
+matmul_eval(A, B,  4, 32, mode=0, Sparsity=True)
+matmul_eval(A, B,  4, 32, mode=1, Sparsity=False)
+matmul_eval(A, B,  4, 32, mode=1, Sparsity=True)
+matmul_eval(A, B,  8, 32, mode=0, Sparsity=False)
+matmul_eval(A, B,  8, 32, mode=0, Sparsity=True)
+matmul_eval(A, B,  8, 32, mode=1, Sparsity=False)
+matmul_eval(A, B,  8, 32, mode=1, Sparsity=True)
+matmul_eval(A, B, 16, 32, mode=0, Sparsity=False)
+matmul_eval(A, B, 16, 32, mode=0, Sparsity=True)
+matmul_eval(A, B, 16, 32, mode=1, Sparsity=False)
+matmul_eval(A, B, 16, 32, mode=1, Sparsity=True)
--- spmmsim/model/hw_model/compute_model/systolic_array/utils/IndexBuffer.py
+++ spmmsim/model/hw_model/compute_model/systolic_array/utils/IndexBuffer.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .SparseMatirx import SparseMatrix
+from SparseMatirx import SparseMatrix
 
 class IndexBuffer:
     def __init__(self,row_num,col_num,systolic_col):
@@ -7,86 +7,87 @@ class IndexBuffer:
         self.col_num = col_num
         self.systolic_col = systolic_col
 
-        self.buffer = np.zeros((row_num,col_num))
-        self.buffer_flag = np.zeros((row_num,col_num)) #记录数据是否有效，1有效，0无效或已经送出
+        self.buffer = np.zeros((row_num, col_num))
+        self.buffer_en = np.zeros((row_num, col_num)) # 记录数据是否有效，1有效，0无效或已经送出
         self.row_ptrs = np.zeros(row_num) # 记录每一行加载到第几个数了
-        self.block_ptrs = 0 #记录运算到哪一块了
-        self.row_finish = np.zeros(row_num) #记录每一行是否已经算完
+        self.block_ptrs = 0 # 记录运算到哪一块了
+        self.row_end = np.zeros(row_num) #记录每一行是否已经算完
 
     def preload_sparse_matrix(self,matrix):
         self.matrix = matrix
 
+    # 从CSR的稀疏矩阵中加载索引和值到缓冲区
     def load_index(self):
         for i in range(self.row_num):
-            row_index = i + self.block_ptrs*self.row_num
-            length = self.matrix.indptr[row_index+1] - self.matrix.indptr[row_index]
+            row_index = i + self.block_ptrs*self.row_num # 第几行
+            length = self.matrix.indptr[row_index+1] - self.matrix.indptr[row_index] # 这一行多少数
             for j in range(self.col_num):
-                if(self.row_ptrs[i] == length):
-                    self.row_finish[i] = 1
+                if (self.row_ptrs[i] == length): # 这一行扫完了
+                    self.row_end[i] = 1
                     break
-                if not self.buffer_flag[i][j]:
+                if not self.buffer_en[i][j]:
                     index = int(self.matrix.indptr[row_index] + self.row_ptrs[i])
+                    self.buffer_en[i][j] = 1
                     self.buffer[i][j] = self.matrix.indices[index]
-                    self.buffer_flag[i][j] = 1
                     self.row_ptrs[i] = self.row_ptrs[i] + 1
 
 
     def clear_buffer(self):
         self.buffer = np.zeros((self.row_num,self.col_num))
-        self.buffer_flag = np.zeros((self.row_num,self.col_num)) #记录数据是否有效，1有效，0无效或已经送出
+        self.buffer_en = np.zeros((self.row_num,self.col_num)) # 记录数据是否有效，1有效，0无效或已经送出
         self.row_ptrs = np.zeros(self.row_num) # 记录每一行加载到第几个数了
-        self.row_finish = np.zeros(self.row_num)
+        self.row_end = np.zeros(self.row_num)
 
-    def launch_inst(self,mode=1):
+    def launch_inst(self, mode=1):
         index_matrix = np.full((self.row_num, self.systolic_col), -1)
 
         col_to_value = {}
         value_to_col = {}
 
-        n,m = self.buffer.shape
+        n, m = self.buffer.shape
         if mode == 0:
             for i in range(n):
                 for j in range(m):
-                    if self.buffer_flag[i][j]:
+                    if self.buffer_en[i][j]:
                         value = int(self.buffer[i][j])
 
                         if value in value_to_col:
                             col_idx = value_to_col[value]
                             index_matrix[i][col_idx] = value
-                            self.buffer_flag[i][j] = 0
+                            self.buffer_en[i][j] = 0
                         else:
                             for col in range(self.systolic_col):
                                 if col not in col_to_value:
                                     col_to_value[col] = value
                                     value_to_col[value] = col
                                     index_matrix[i][col] = value
-                                    self.buffer_flag[i][j] = 0
+                                    self.buffer_en[i][j] = 0
                                     break
                                 elif col_to_value[col] == value:
                                     index_matrix[i][col] = value
-                                    self.buffer_flag[i][j] = 0
+                                    self.buffer_en[i][j] = 0
                                     break
         else:
             for j in range(m):
                 for i in range(n):
-                    if self.buffer_flag[i][j]:
+                    if self.buffer_en[i][j]:
                         value = int(self.buffer[i][j])
 
                         if value in value_to_col:
                             col_idx = value_to_col[value]
                             index_matrix[i][col_idx] = value
-                            self.buffer_flag[i][j] = 0
+                            self.buffer_en[i][j] = 0
                         else:
                             for col in range(self.systolic_col):
                                 if col not in col_to_value:
                                     col_to_value[col] = value
                                     value_to_col[value] = col
                                     index_matrix[i][col] = value
-                                    self.buffer_flag[i][j] = 0
+                                    self.buffer_en[i][j] = 0
                                     break
                                 elif col_to_value[col] == value:
                                     index_matrix[i][col] = value
-                                    self.buffer_flag[i][j] = 0
+                                    self.buffer_en[i][j] = 0
                                     break
         # print(col_to_value)
         # print(index_matrix)
@@ -94,7 +95,7 @@ class IndexBuffer:
         for key,value in col_to_value.items():
             index_B[key] = value
 
-        return index_matrix,index_B
+        return index_matrix, index_B
 
 
     
