4e466a2daf82fccc7ef792b4699c5d16ba9a5e6c
shiroha <whmio0115@gmail.com>
2024-09-24 16:55:23 +0800

    [WIP]

--- spmmsim/model/hw_model/memory_model/cache/interface.py
+++ spmmsim/model/hw_model/memory_model/cache/interface.py
@@ -1,7 +1,45 @@
-import numpy as np
+import argparse
+import os
+import configparser as cp
+
+from cachesim import CacheSim
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', metavar='Config file', type=str,
+                        default="../scale.cfg",
+                        help="Path to the config file")
+args   = parser.parse_args()
+    
+config = args.c
+if not os.path.exists(config):
+    print("ERROR: Config file not found") 
+    print("Input file:" + config)
+    print('Exiting')
+    exit()
+else: 
+    config_file = config
+
+config = cp.ConfigParser()
+config.read(config_file)
+
+section = 'cache_architecture_presets'
+size            = int(config.get(section, 'Total Size'))
+cacheline_size  = int(config.get(section, 'Cacheline Size'))
+way             = int(config.get(section, 'Way of Associativity'))
+replacement     = str(config.get(section, 'Way of Replacement'))
+data_size       = int(config.get(section, 'Data Size'))
+miss_latency    = int(config.get(section, 'latency'))
+
+sim = CacheSim()
+sim.set_params(way, size, cacheline_size, replacement, 
+               addr_size=32, data_size=data_size)
+for i in range(0, 1000, 1):
+    sim.cache_read(i, 0, 0, 0, True)
+        # sim.sim.print_info()
+sim.sim.print_info()
 
 # 返回memory_access hit或者miss
-def memory_access_eval(matrix_A, matrix_B, systolic_row_opt=-1, systolic_col_opt=-1, 
-                buffer_col_opt=-1, mode=0, Sparsity=False):
+def memory_access_eval(raddr):
     if_hit = False
-    return if_hit
+    latency = 0
+    return if_hit, latency
